dcc8f968250e1f8c16a5cf72999ff4ca131fa7b6
UmarADev <umargithub03@gmail.com>
2024-07-01 19:21:57 +0500

    Add project files.

---
+++ GuidGeneratorWithUTC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GuidGeneratorWithUTC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userChoise = "";
+
+            do
+            {
+                string guidGenerator = Guid.NewGuid().ToString();
+
+               
+            } while (userChoise == "y");
+        }
+    }
+}
+
